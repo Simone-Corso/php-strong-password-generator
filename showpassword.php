6c7fb1eb0d044d051e68072316a0605927f5ce20
Simone-Corso <scorso26@gmail.com>
2024-01-17 16:04:08 +0100

    nel showpassword ho inserito la funzione per vedere la password

---
+++ showpassword.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+// Verifica se la password è presente nella sessione
+if (isset($_SESSION["generatedPassword"])) {
+    $generatedPassword = $_SESSION["generatedPassword"];
+    unset($_SESSION["generatedPassword"]);
+
+    // Mostrerà la password
+    echo "<p>La tua password generata è: $generatedPassword</p>";
+} else {
+    // Se non presenta la password ritorna alla pagina principale
+    header("Location: index.php");
+}
+?>
