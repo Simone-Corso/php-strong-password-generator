e7e58e0617a35d0447b15a6c0837e83879a226c7
Simone-Corso <scorso26@gmail.com>
2024-01-17 15:40:02 +0100

    sono riuscito a capire l'errore, quindi ora funziona perfettamente il gerare password

--- index.php
+++ index.php
@@ -32,12 +32,20 @@
             //stampo la password generata
 
             echo "<p> La tua password generata è: $password<p>";
-    
-            header("Location: index.php");
-            exit();
         }
     }
-
+    
+        function generateRandomPassword($length) {
+            $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+            $password = '';
+            
+            for ($i = 0; $i < $length; $i++) {
+                $password .= $characters[random_int(0, strlen($characters) - 1)];
+            }
+        
+            return $password;
+        }
+        ?>
 
     ?>
 
