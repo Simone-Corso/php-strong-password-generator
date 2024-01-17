45824b8e613b11940db2b3289fcd46352a76940b
Simone-Corso <scorso26@gmail.com>
2024-01-17 15:10:55 +0100

    ho creato il file index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Generatore Di Password</title>
+</head>
+<body>
+
+<h1>Generatore Di Password</h1>
+    
+</body>
+</html>
