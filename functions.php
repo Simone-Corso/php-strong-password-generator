06bb71e426f1e31571e9e2fd245b4f3bdc7ed287
Simone-Corso <scorso26@gmail.com>
2024-01-17 15:44:23 +0100

    ho creato il file functions e ho sposato la funzione che poi viene richiamata da include

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php 
+
+function generateRandomPassword($length) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+    $password = '';
+    
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $characters[random_int(0, strlen($characters) - 1)];
+    }
+
+    return $password;
+}
+?>
--- index.php
+++ index.php
@@ -22,6 +22,8 @@
 
     session_start();
 
+    include 'functions.php';
+
     if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
         if (isset($_GET["passwordLength"])) {
@@ -33,19 +35,7 @@
 
             echo "<p> La tua password generata è: $password<p>";
         }
-    }
-    
-        function generateRandomPassword($length) {
-            $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
-            $password = '';
-            
-            for ($i = 0; $i < $length; $i++) {
-                $password .= $characters[random_int(0, strlen($characters) - 1)];
-            }
-        
-            return $password;
-        }
-        ?>
+    };
 
     ?>
 
